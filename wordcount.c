e59e56e7f1c574f997f794f051a1bca804ec7151
saurabhk3 <kannaujia944@gmail.com>
2020-08-10 19:45:07 +0530

    All working from stdin

--- wordcount.c
+++ wordcount.c
@@ -7,15 +7,32 @@
 #include<errno.h>
 
 void readFromStdin();
-
+void  printOptionStdin(char*);
 
 unsigned long int word = 0, ch = 0, line = 0, maxLenLine = 0; 
 int main(int argc, char*argv[]){
-    // 1. read from stdin
-    if(argc == 1){
+    char *options;
+    if(argc == 1){  // read from stdin with no option provided
         readFromStdin();
+        printf("\t%u\t%u\t%u\n",line, word,ch);
+    }else if(argc==2){ // possibilities: 1. stdin with -,2. stdin with -[option] 3. read from a file, 4. '*' :read all files in dir
+        
+        options = argv[1];
+        if(strcmp("-",options)==0){
+            // 1: stdin with -
+            readFromStdin();
+            printf("\t%u\t%u\t%u -\n",line, word,ch);
+        }else if(argv[1][0] == '-' && strlen(argv[1]) > 1){
+            // 2: stdin with -[option]
+            readFromStdin();
+            printOptionStdin(options);
+        }else if((strstr("-",options))==NULL){
+            // 3 .file
+        }else if(strstr("*",options) != NULL){
+            // 4. *
+        }
     }
-    printf("\t%u %u %u\n",line, word,ch);
+    
 }
 
 void readFromStdin(){
@@ -43,4 +60,72 @@ void readFromStdin(){
         maxLenLine = currentLenLine;
     }
     return;
+}
+void printOptionStdin(char*s){ // TODO add combiantions of lwmLc
+    if(s[0]=='-'){
+        if(strcmp(s,"-c")==0 || strcmp(s,"-m")==0){
+            printf("\t%ld\n",ch);
+        }else if(strcmp(s,"-l")==0){
+            printf("\t%ld\n",line);
+        }else if(strcmp(s,"-L")==0){
+            printf("\t%ld\n",maxLenLine);
+        }else if(strcmp(s,"-w")==0){
+            printf("\t%ld\n",word);
+        }else if(strcmp("-cwlL",s)==0 || strcmp("-cwLl",s)==0 || strcmp("-clwL",s)==0 || strcmp("-clLw",s)==0 || 
+            strcmp("-cLwl",s)==0 || strcmp("-cLlw",s)==0 || strcmp("-Lcwl",s)==0 || strcmp("-Lclw",s)==0 ||
+            strcmp("-Llcw",s)==0 || strcmp("-Llwc",s)==0 || strcmp("-Lwcl",s)==0 || strcmp("-Lwlc",s)==0 ||
+            strcmp("-lcwL",s)==0 || strcmp("-lcLw",s)==0 || strcmp("-lLcw",s)==0 || strcmp("-lLwc",s)==0 ||
+            strcmp("-lwcL",s)==0 || strcmp("-lwLc",s)==0 || strcmp("-wlLc",s)==0 || strcmp("-wlcL",s)==0 ||
+            strcmp("-wLcl",s)==0 || strcmp("-wLlc",s)==0 || strcmp("-wcLl",s)==0 || strcmp("-wclL",s)==0 ||
+            strcmp("-cmlL",s)==0 || strcmp("-cmLl",s)==0 || strcmp("-clmL",s)==0 || strcmp("-clLm",s)==0 || 
+            strcmp("-cLml",s)==0 || strcmp("-cLlm",s)==0 || strcmp("-Lcml",s)==0 || strcmp("-Lclm",s)==0 ||
+            strcmp("-Llcm",s)==0 || strcmp("-Llmc",s)==0 || strcmp("-Lmcl",s)==0 || strcmp("-Lmlc",s)==0 ||
+            strcmp("-lcmL",s)==0 || strcmp("-lcLm",s)==0 || strcmp("-lLcm",s)==0 || strcmp("-lLmc",s)==0 ||
+            strcmp("-lmcL",s)==0 || strcmp("-lmLc",s)==0 || strcmp("-mlLc",s)==0 || strcmp("-mlcL",s)==0 ||
+            strcmp("-mLcl",s)==0 || strcmp("-mLlc",s)==0 || strcmp("-mcLl",s)==0 || strcmp("-mclL",s)==0){
+
+            printf("\t%ld %ld %ld %ld\n",line,word,ch,maxLenLine);
+        }else if(strcmp("-clw",s)==0 || strcmp("-cwl",s)==0 || strcmp("-lcw",s)==0 || strcmp("-lwc",s)==0 || 
+            strcmp("-wcl",s)==0 || strcmp("-wlc",s)==0){
+
+            printf("\t%ld %ld %ld\n",line,word,ch);
+        }else if(strcmp("-cL",s)==0 || strcmp("-Lc",s)==0){
+            printf("\t%ld %ld\n",ch,maxLenLine);
+        }else if(strcmp("-cl",s)==0 || strcmp("-lc",s)==0){
+            printf("\t%ld %ld\n",line,ch);
+        }else if(strcmp("-cw",s)==0 || strcmp("-wc",s)==0){
+            printf("\t%ld %ld\n",word,ch);
+        }else if(strcmp("-cm",s)==0 || strcmp("-mc",s)==0){
+            printf("\t%ld %ld\n",ch,ch);
+        }else if(strcmp("-cc",s)==0){
+            printf("\t%ld %ld\n",ch);
+        }else if(strcmp("-ml",s)==0 || strcmp("-lm",s)==0){
+            printf("\t%ld %ld\n",line,ch);
+        }else if(strcmp("-mL",s)==0 || strcmp("-Lm",s)==0){
+            printf("\t%ld %ld\n",ch,maxLenLine);
+        }else if(strcmp("-mw",s)==0 || strcmp("-wm",s)==0){
+            printf("\t%ld %ld\n",word,ch);
+        }else if(strcmp("-lL",s)==0 || strcmp("-Ll",s)==0){
+            printf("\t%ld %ld\n",line,maxLenLine);
+        }else if(strcmp("-lw",s)==0 || strcmp("-wl",s)==0){
+            printf("\t%ld %ld\n",line,word);
+        }else if(strcmp("-Lw",s)==0 || strcmp("-wL",s)==0){
+            printf("\t%ld %ld\n",word,maxLenLine);
+        }
+        else if(s[1]=='-'){
+            if(strcmp("--chars",s)==0 ||strcmp("--bytes",s)==0 ){
+                printf("\t%ld\n",ch);
+            }else if(strcmp("--lines",s)==0){
+                printf("\t%ld\n",line);
+            }else if(strcmp("--max-line-length",s)==0){
+                printf("\t%ld\n",maxLenLine);
+            }else if(strcmp("--words",s)==0){
+                printf("\t%ld\n",word);
+            }else{
+                printf("\tInvalid input\n");
+            }
+        }
+    }else{
+        printf("\tInvalid input\n");
+    }
 }
